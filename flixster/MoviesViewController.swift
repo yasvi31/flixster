e9ecef02e2a99e5b7909d1d0575b09796190a86c
Yasvi Patel <patelya@uci.edu>
2020-04-19 18:44:25 -0700

    Added Movie "see more details" feature

--- flixster/MoviesViewController.swift
+++ flixster/MoviesViewController.swift
@@ -64,14 +64,23 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //     Get the new view controller using segue.destination.
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+    //     Pass the selected object to the new view controller.
+        let detailVC = segue.destination as! MovieDetailsViewController
+        detailVC.movie = movie
+    
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
+    
 
 }
